ed56b7ae39cfd57db792f440379645141d2c1045
suReZj <1014652997@qq.com>
2017-02-22 14:46:39 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/sure/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package sure.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dell88 on 2017/2/22 0022.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
